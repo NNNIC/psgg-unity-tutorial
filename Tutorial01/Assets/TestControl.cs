46d7a78046be673f3909728030763e70de375d98
nn4343 <gea01441@nifty.com>
2018-07-22 11:16:25 +0900

    リファクタリング

---
+++ Tutorial01/Assets/TestControl.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+public partial class TestControl  {
+		
+	// write your code 
+
+	void create_cube()
+	{
+		GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+	}
+}
---
+++ Tutorial01/Assets/TestControl_created.cs
@@ -0,0 +1,61 @@
+﻿// psggConverterLib.dll converted from TestControl.xlsx. 
+public partial class TestControl : StateManager {
+
+    public void Start()
+    {
+        Goto(S_START);
+    }
+
+
+    /*
+        S_START
+    */
+    void S_START(bool bFirst)
+    {
+        if (bFirst)
+        {
+        }
+        if (!HasNextState())
+        {
+            SetNextState(S_CREATE_CUBE);
+        }
+        if (HasNextState())
+        {
+            GoNextState();
+        }
+    }
+    /*
+        S_END
+    */
+    void S_END(bool bFirst)
+    {
+        if (bFirst)
+        {
+        }
+        if (HasNextState())
+        {
+            GoNextState();
+        }
+    }
+    /*
+        S_CREATE_CUBE
+        キューブ作成
+    */
+    void S_CREATE_CUBE(bool bFirst)
+    {
+        if (bFirst)
+        {
+            create_cube();
+        }
+        if (!HasNextState())
+        {
+            SetNextState(S_END);
+        }
+        if (HasNextState())
+        {
+            GoNextState();
+        }
+    }
+
+}
+
