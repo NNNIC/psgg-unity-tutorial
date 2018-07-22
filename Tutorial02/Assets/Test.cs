db6be23cf763b99aac45a22f40a09e50cb41a69f
nn4343 <gea01441@nifty.com>
2018-07-22 22:07:08 +0900

    Tutorial 02 追加

---
+++ Tutorial02/Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	TestControl m_sm;
+
+	// Use this for initialization
+	void Start () {
+		m_sm = new TestControl();
+		m_sm.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_sm.update();		
+	}
+}
---
+++ Tutorial02/Assets/TestControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+public partial class TestControl  {
+		
+	// write your code 
+
+	void create_cube()
+	{
+		GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+	}
+
+	int m_val;
+	void set_zero_or_one()
+	{
+		m_val = Random.Range(0,2);
+	}
+
+	void br_zero(System.Action<bool> st)
+	{
+		if (m_val == 0)
+		{
+			SetNextState(st);
+		}
+	}
+	void br_one(System.Action<bool> st)
+	{
+		if (m_val == 1)
+		{
+			SetNextState(st);
+		}
+	}
+
+	void create_sphere()
+	{
+		var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		go.transform.position = Vector3.up;
+	}
+	void create_cylinder()
+	{
+		var go = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		go.transform.position = Vector3.up;
+	}
+}
